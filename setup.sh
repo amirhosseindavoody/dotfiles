008a137f6eb027ca9e4eac37a04c6e6e8a9bd1ef
Amirhossein Davoody <amirhossein.davoody@gmail.com>
2025-05-15 12:48:15 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,9 +18,9 @@ export UV_CACHE_DIR="$(pwd)/.cache"
 export UV_PYTHON_INSTALL_DIR="$(pwd)/.python"
 
 if [ "$#" -eq 2 ]; then
-    ./uv run src/init.py --config "$1" --workspace "$2"
+    ./uv run --python 3.12 src/init.py --config "$1" --workspace "$2"
 else
-    ./uv run src/init.py --config config.yaml --workspace ~/workspace
+    ./uv run --python 3.12 src/init.py --config config.yaml --workspace ~/workspace
 fi
 
 rm -rf "$UV_CACHE_DIR"
